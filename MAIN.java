6eeb926a57ef8b814b2ce20e447666021bf5e624
Hyounggon <cokeneon@gmail.com>
2024-10-19 14:21:20 +0900

    이클립스 사용하는거 포기하고 git bash로 다시 복귀...

---
+++ MAIN.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main{
+        public static void obesity(double tall, double weight){
+                double standard, obe_val;
+
+                if(tall<150){   standard = tall - 100;}
+                else if(tall>=160){ standard = (double)((tall-100)*0.9);}
+                else{ standard = (tall-150)/2 + 50;}
+
+                obe_val = (double)((weight - standard)*100/standard);
+
+                if(obe_val<=10){System.out.printf("정상\n");}
+                else if(obe_val>20){System.out.printf("비만\n");}
+                else{System.out.printf("과체중\n");}
+        }
+
+        
+        public static void main(String[] args){
+                double tall, weight;
+                Scanner input = new Scanner(System.in);
+                tall = input.nextInt(); //입력받기
+                weight = input.nextInt();
+                input.close();
+
+                obesity(tall, weight);
+        }
+}
+
