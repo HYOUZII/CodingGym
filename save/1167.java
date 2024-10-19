65d13af4ee3f6d5102661e11bb9b9d89d112fde3
Hyounggon <cokeneon@gmail.com>
2024-10-19 14:46:32 +0900

    2024.10.19] java 공부 끝!, 마지막 MAIN은 비교메서드 동작구현

--- save/1167.java
+++ save/1167.java
@@ -1,24 +1,24 @@
-import java.util.*;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int first = 0;
-        int second = 0;
-        for (int i=0 ; i<3 ;i++) {
-            int num = input.nextInt();
-            if (num > first) {
-                second = first;
-                first = num;
-            }
-            else if(num > second){
-                second = num;
-            }
-        }
-        
-        input.close();
-
-        System.out.print(second);
-
-    }
-}
+//import java.util.*;
+//
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner input = new Scanner(System.in);
+//        int first = 0;
+//        int second = 0;
+//        for (int i=0 ; i<3 ;i++) {
+//            int num = input.nextInt();
+//            if (num > first) {
+//                second = first;
+//                first = num;
+//            }
+//            else if(num > second){
+//                second = num;
+//            }
+//        }
+//        
+//        input.close();
+//
+//        System.out.print(second);
+//
+//    }
+//}
