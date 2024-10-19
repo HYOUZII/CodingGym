db222e8b6d8868676dbf4aead6288d7957e8844e
Hyounggon <cokeneon@gmail.com>
2024-10-19 14:50:42 +0900

    다시 저장

--- MAIN.java
+++ MAIN.java
@@ -1,31 +1,31 @@
 import java.util.*;
 
 class Main{
-        public static void obesity(double tall, double weight){
-                double standard, obe_val;
+    public static int largenum(int X, int Y){
+        int  Large;
+        Large = X > Y ? X : Y;
+        return Large;
+    }
+    public static int smallnum(int X, int Y){
+        int  Small;
+        Small = X < Y ? X : Y;
+        return Small;
+    }
 
-                if(tall<150){   standard = tall - 100;}
-                else if(tall>=160){ standard = (double)((tall-100)*0.9);}
-                else{ standard = (tall-150)/2 + 50;}
+    public static boolean sameNum(int X, int Y){
+        // boolean  Same;
+        // Same = X == Y ? true : false;
+        return X==Y;
+    }
+    public static void main(String[] args){
+        int A, B;
+        Scanner input = new Scanner(System.in);
+            A = input.nextInt();
+            B = input.nextInt();
+        input.close();
 
-                obe_val = (double)((weight - standard)*100/standard);
-
-                if(obe_val<=10){System.out.printf("정상\n");}
-                else if(obe_val>20){System.out.printf("비만\n");}
-                else{System.out.printf("과체중\n");}
-        }
-
-        
-        public static void main(String[] args){
-                double tall, weight;
-                Scanner input = new Scanner(System.in);
-                tall = input.nextInt(); //입력받기
-                weight = input.nextInt();
-                input.close();
-
-                obesity(tall, weight);
-        }
+        System.out.println(largenum(A,B));
+        System.out.println(smallnum(A,B));
+        System.out.println(sameNum(A,B));
+    }
 }
-
-
-
